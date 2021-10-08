f016c2fb63e35cd908b4a4449b80ca04bed7c1f2
Isaac Luis Silva Santos <shurikenops@gmail.com>
2021-10-08 07:45:19 -0300

    Atividades

--- JavaFX090602/src/main/TecladoCadastroController.java
+++ JavaFX090602/src/main/TecladoCadastroController.java
@@ -1,27 +1,68 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package main;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
 
-/**
- * FXML Controller class
- *
- * @author prjlu
- */
 public class TecladoCadastroController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private DatePicker datePickerDataNascimento;
+    @FXML
+    private ChoiceBox<String> comboBoxClasses;
+    @FXML
+    private PasswordField passwordFieldSenha;
+    @FXML
+    private CheckBox checkBoxConcordo;
+    @FXML
+    private Button buttonConcluir;
+    @FXML
+    private PasswordField passwordFieldSenha2;
+    @FXML
+    private TextField textFieldNick;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        String[] classes = {"Arqueiro", "Bárbaro", "Cavaleiro", "Guerreiro", "Mago", "Necromante", "Paladino", "Sacerdote"};
+        comboBoxClasses.getItems().addAll(classes);
+    }
     
+    @FXML
+    private void cadastrarPessoa(ActionEvent event) throws IOException {
+        String nome = textFieldNick.getText();
+        String senha = passwordFieldSenha.getText();
+        String estado = comboBoxClasses.getValue();
+        String dataNasc = datePickerDataNascimento.getValue().toString();
+        System.out.println("Cadastro ralizado!");
+        System.out.println("Nick: " + nome);
+        System.out.println("Senha: " + senha);
+        System.out.println("Classe: " + estado);
+        System.out.println("Nasc. : " + dataNasc);
+        
+        File arquivo = new File("player.txt");
+        arquivo.createNewFile();
+        FileWriter escrever =  new FileWriter(arquivo.getName(), true);
+        escrever.write("Player Nick: "+nome+" | Classe: "+estado+" | Data de Nasc.: "+dataNasc+" | Senha: "+senha);
+        escrever.close();
+    }
+    
+    @FXML
+    private void habilitarCadastro(ActionEvent event) {
+        String senha1, senha2;
+        senha1 = passwordFieldSenha.getText();
+        senha2 = passwordFieldSenha2.getText();
+        buttonConcluir.setDisable(senha1!=senha2);
+    }
 }
--- aula1906/src/aula1906/desafio3.java
+++ aula1906/src/aula1906/desafio3.java
@@ -1,7 +1,54 @@
 package aula1906;
+
+import java.util.Scanner;
+
 public class desafio3 {
+    
+    static byte maximoDias[] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+    
     public static void main(String[] args) {
         
+        System.out.println("Informe a data no formato dd/mm/aaaa:");
+        Scanner teclado = new Scanner(System.in);
+        String data[] = teclado.nextLine().split("/");
+        byte dia = Byte.parseByte(data[0]);
+        byte mes = Byte.parseByte(data[1]);
+        int ano = Integer.parseInt(data[2]);
+        if (dataValida(dia, mes, ano)) {
+            System.out.println("Data válida");
+        } else { 
+            System.out.println("Data Inválida");
+        }
+        
+        // Pede para o usuário fornecer uma data no padrão dd/mm/aaaa
+        // O método split() pode ser útil
+        // Retorna se a data é válida ou não
+        
     }
-
+    
+    private static boolean dataValida(byte dia, byte mes, int ano) {
+        if (dia < 1 || mes < 1 || ano < 1 || mes > 12) {
+            return false;
+        } else if (mes == 2 && anoBissexto(ano)) {
+            return dia<=29;
+        } else {
+            return dia<=maximoDias[mes];
+        }
+    }
+            
+    private static boolean anoBissexto(int ano) {
+        
+        // 1. todo ano divisível por 400 é bissexto
+        // 2. todo ano divisível por 100 não é bissexto
+        // 3. ano bissexto é aquele divisível por 4
+        
+        if (ano%400==0) {
+            return true;
+        } else if (ano%100==0) {
+            return false;
+        } else {
+            return ano%4==0;
+        }
+        
+    }
 }
