4aa64308fc3582e5b5942e744ea4473bcb87d0e9
Isaac Luis Silva Santos <shurikenops@gmail.com>
2021-10-19 07:37:11 -0300

    Pratica21

---
+++ Eclipse/Pratica21/src/PraticaVinteUm.java
@@ -0,0 +1,21 @@
+
+public class PraticaVinteUm {
+
+	public static void main(String[] args) {
+
+		int i=1, x=1;
+
+		do {
+
+			if(i%2!=0) {
+				x++;
+				System.out.print(i+" ");
+			}
+
+			i++;
+
+		} while(x!=20);
+
+	}
+
+}
