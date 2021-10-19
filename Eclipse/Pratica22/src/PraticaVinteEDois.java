bd0ce3ccdd5b5808894b9d1f8bc3547bb745fe81
Isaac Luis Silva Santos <shurikenops@gmail.com>
2021-10-19 07:41:03 -0300

    Pratica22

---
+++ Eclipse/Pratica22/src/PraticaVinteEDois.java
@@ -0,0 +1,23 @@
+
+public class PraticaVinteEDois {
+
+	public static void main(String[] args) {
+
+		int i=1, x=1;
+		
+		System.out.print(i+" ");
+		
+		do {
+
+			if(i%2==0) {
+				x++;
+				System.out.print(i+" ");
+			}
+
+			i++;
+
+		} while(x!=12);
+
+	}
+
+}
