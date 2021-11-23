3ebbb019b0a3dc8ac41ec9e59b0e9d3a7e8f82c4
Isaac Luis Silva Santos <shurikenops@gmail.com>
2021-11-23 09:12:20 -0300

    Update Pratica31.java

--- Eclipse/Pratica31/src/Pratica31.java
+++ Eclipse/Pratica31/src/Pratica31.java
@@ -1,10 +1,10 @@
 public class Pratica31 {
 
 	public static void main(String[] args) {
-		imprimir(sequencia01());
+		imprimir(sequencia());
 	}
 
-	public static int[] sequencia01() {
+	public static int[] sequencia() {
 		int[] seq = new int[15];
 		for (int i = 0, num = 2; i < seq.length; i++) {
 			if(num%2==0) {
